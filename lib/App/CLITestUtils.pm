ca5f78da5589ab948545ebac4fcc45023ab42832
perlancar (@pc-office) <perlancar@gmail.com>
2016-11-01 23:36:48 +0700

    Initial commit

---
+++ lib/App/CLITestUtils.pm
@@ -0,0 +1,18 @@
+package App::CLITestUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to testing/debugging in CLI environment
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
