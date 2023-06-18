141d49a412b9b77fff633cd0ea67cdebd2068c6a
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-06-18 18:15:13 +0700

    Rename CLI csv-pick-cell-values to csv-pick-cells

--- lib/App/CSVUtils/csv_pick_cells.pm
+++ lib/App/CSVUtils/csv_pick_cells.pm
@@ -1,4 +1,4 @@
-package App::CSVUtils::csv_pick_cell_values;
+package App::CSVUtils::csv_pick_cells;
 
 use 5.010001;
 use strict;
@@ -14,8 +14,8 @@ use App::CSVUtils qw(
                 );
 
 gen_csv_util(
-    name => 'csv_pick_cell_values',
-    summary => 'Get one or more random cell values from CSV',
+    name => 'csv_pick_cells',
+    summary => 'Get one or more random cells from CSV',
     description => <<'_',
 
 The values will be returned as a 1-column CSV.
@@ -50,7 +50,7 @@ _
         my $r = shift;
 
         # we add this key to the stash
-        $r->{picked_cell_values} = [];
+        $r->{picked_cells} = [];
         $r->{num_cells} = 0;
 
         $r->{output_fields} = ['value'];
@@ -63,22 +63,22 @@ _
         if ($r->{util_args}{num_cells} == 1) {
             for my $i (0 .. $#{ $r->{input_fields} }) {
                 # algorithm from Learning Perl
-                $r->{picked_cell_values}[0] = $r->{input_row}[$i]
+                $r->{picked_cells}[0] = $r->{input_row}[$i]
                     if rand(++$r->{num_cells}) < 1;
             }
         } else {
             for my $i (0 .. $#{ $r->{input_fields} }) {
                 $r->{num_cells}++;
                 # algorithm from Learning Perl, modified
-                if (@{ $r->{picked_cell_values} } < $r->{util_args}{num_cells}) {
+                if (@{ $r->{picked_cells} } < $r->{util_args}{num_cells}) {
                     # we haven't reached $num_cells, put cell to result in a
                     # random position
-                    splice @{ $r->{picked_cell_values} }, rand(@{ $r->{picked_cell_values} }+1), 0, $r->{input_row}[$i];
+                    splice @{ $r->{picked_cells} }, rand(@{ $r->{picked_cells} }+1), 0, $r->{input_row}[$i];
                 } else {
                     # we have reached $num_cells, just replace an item randomly,
                     # using algorithm from Learning Perl, slightly modified
-                    rand($r->{num_cells}) < @{ $r->{picked_cell_values} }
-                        and splice @{ $r->{picked_cell_values} }, rand(@{ $r->{picked_cell_values} }), 1, $r->{input_row}[$i];
+                    rand($r->{num_cells}) < @{ $r->{picked_cells} }
+                        and splice @{ $r->{picked_cells} }, rand(@{ $r->{picked_cells} }), 1, $r->{input_row}[$i];
                 }
             }
         }
@@ -87,7 +87,7 @@ _
     after_read_input => sub {
         my $r = shift;
 
-        for my $value (@{ $r->{picked_cell_values} }) {
+        for my $value (@{ $r->{picked_cells} }) {
             $r->{code_print_row}->([$value]);
         }
     },
